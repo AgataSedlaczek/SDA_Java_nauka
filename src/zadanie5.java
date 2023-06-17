ed5c3a5f61d906ece7765fddac26d2466d0f93c2
AgataSedlaczek <navi2@o2.pl>
2023-06-17 12:48:13 +0200

    Java

---
+++ src/zadanie5.java
@@ -0,0 +1,9 @@
+public class zadanie5 {
+    public static void main(String[] args) {
+        for (int i= 10; i <= 20; i = i+2 ){
+            System.out.println("liczby parzyste: " + i);
+        }
+    }
+}
+
+
---
+++ src/zadanie6.java
@@ -0,0 +1,7 @@
+public class zadanie6 {
+    public static void main(String[] args) {
+        for (int j=0, j <= 10, j = j ^ 0++){
+            System.out.println("Potęgi " + j);
+        }
+    }
+}
