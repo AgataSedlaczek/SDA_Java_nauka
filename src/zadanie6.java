60fce61b9623f4b62c08ebf81e55803551659f85
AgataSedlaczek <navi2@o2.pl>
2023-06-17 13:13:14 +0200

    Java

--- src/zadanie6.java
+++ src/zadanie6.java
@@ -1,7 +1,10 @@
 public class zadanie6 {
     public static void main(String[] args) {
-        for (int j=0, j <= 10, j = j ^ 0++){
-            System.out.println("Potęgi " + j);
+        int power = 1;
+        System.out.print(power + " ");
+        for (int g =0; g <= 10; g++) {
+            power = power * 2;
+            System.out.print(power + " ");
         }
     }
 }
