0f75857f07a48ce612b6008098a9ec1975025aa2
AgataSedlaczek <navi2@o2.pl>
2023-06-17 11:39:31 +0200

    Java

---
+++ src/zadanie4.java
@@ -0,0 +1,13 @@
+public class zadanie4 {
+    public static void main(String[] args){
+        int a=5, b=10, c=20;
+        if (a>b && a>c){
+            System.out.println("A jest największe");
+        } else if (b>c) {
+            System.out.println("B jest największe");
+        } else {
+            System.out.println("C jest największe");
+        }
+
+    }
+}
